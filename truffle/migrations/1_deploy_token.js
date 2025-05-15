f9011b49aed8e12ef33ce0cfb7511629c3169f17
0xyzabc <theprince.yis9@gmail.com>
2025-05-15 08:20:47 +0700

    hanya menggunakan hardhat untuk deploy

--- truffle/migrations/1_deploy_token.js
+++
@@ -1,6 +0,0 @@
-const MyToken = artifacts.require("MyToken");
-
-module.exports = function (deployer) {
-  const initialSupply = 1000000; // Example initial supply
-  deployer.deploy(MyToken, initialSupply);
-}; 
